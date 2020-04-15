c05d0104453e0f8cb8f89920f0d4c31ce5ae102b
Angel Juarez <angeljuarez729@gmail.com>
2020-04-15 10:40:49 -0400

    imported and rendered NavBar component

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar.js';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <NavBar />
         </header>
       </div>
     );
